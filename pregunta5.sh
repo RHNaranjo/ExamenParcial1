270e9585fd9d6072e9d5f58d9da8ad2ba2fdb066
Rodrigo Naranjo <rodrigonaranjo@Rodrigos-MacBook-Pro.local>
2026-02-23 14:11:55 -0600

    pregunta 5

---
+++ pregunta5.sh
@@ -0,0 +1,96 @@
+# Borré lo que venía antes... jeje :) (creo que tengo que limpiar esto de vez en cuando, había comandos del 2022...)
+ 1499  ifconfig en0
+ 1500  ip a
+ 1501  ip -a
+ 1502  nmap -sS -p 1-100 172.29.33.255
+ 1503  cd Desktop/Universidad\ Panamericana/Clases/1262/Sistemas\ Operativos/
+ 1504  ls
+ 1505  cd t1
+ 1506  cd T1-Open_Write_Close
+ 1507  ls
+ 1508  cat main.cpp
+ 1509  nvim main.cpp
+ 1510  git add main.cpp
+ 1511  git commit -m "hola"
+ 1512  git push
+ 1513  cat main.cpp
+ 1514  cd ../../
+ 1515  cd ..
+ 1516  cd 1252/Compiladores
+ 1517  ls
+ 1518  cd EncriptadorGenomico
+ 1519  ls
+ 1520  CDPATH= ~
+ 1521  cd Documents
+ 1522  ls
+ 1523  cd Flex\ y\ Bison
+ 1524  omz update
+ 1525  clear
+ 1526  cd /Users/rodrigonaranjo/Desktop/Universidad\ Panamericana/Clases/1262/Sistemas\ Operativos/Programas/Concurrencia 
+ 1527  touch hello_concurrency.cpp
+ 1528  cd Desktop/Universidad\ Panamericana/Clases/1262/Sistemas\ Operativos/Proyecto_Router
+ 1529  git clone https://github.com/RHNaranjo/Router_Sistemas_Operativos
+ 1530  cd Router_Sistemas_Operativos
+ 1531  code
+ 1532  cd /Users/rodrigonaranjo/Desktop/Universidad\ Panamericana/Clases/1262/Sistemas\ Operativos/Proyecto_Router/Router_Sistemas_Operativos 
+ 1533  mkdir src include
+ 1534  touch src/main.cpp
+ 1535  touch src/router_cli.cpp src/router_core.cpp
+ 1536  touch include/router_cli.hpp include/router_core.hpp
+ 1537  mkdir docs
+ 1538  touch docs/plan_routing_router.md
+ 1539  ls -r
+ 1540  ls r
+ 1541  man ls
+ 1542  ls -R
+ 1543  git status
+ 1544  git add .
+ 1545  git commit -m "Estructura proyecto"
+ 1546  git push origin main
+ 1547  git add include/router_cli.hpp
+ 1548  git commit -m "Estructura arbol de comandos"
+ 1549  git push
+ 1550  git add include/router_cli.hpp src/router_cli.cpp
+ 1551  git commit -m "Router CLI arbol comandos"
+ 1552  git push
+ 1553  git add include/router_cli.hpp src/router_cli.cpp
+ 1554  git commit -m "Inicio cli"
+ 1555  git push
+ 1556  git add include/router_cli.hpp
+ 1557  git commit -m "."
+ 1558  git push
+ 1559  git add inclide/* src/router_cli.cpp
+ 1560  git add include/* src/router_cli.cpp
+ 1561  git commit -m "Inicios router core, router cli con core básico y comandos"
+ 1562  git push
+ 1563  git add .vscode
+ 1564  git commit -m "."
+ 1565  git push
+ 1566  git add include/* src/router*
+ 1567  git commit -m "Handlers y cambios core"
+ 1568  git push
+ 1569  cd Desktop
+ 1570  cd ..
+ 1571  mkdir ExamenParcial1
+ 1572  cd ExamenParcial1
+ 1573  git init
+ 1574  g++ -std=c++20 -pthread -o preguntaMultiThreads texto_plano_threads.cpp ./multi_threads
+ 1575  g++ -std=c++20 -pthread -o preguntaMultiThreads texto_plano_threads.cpp ./texto_plano_threads
+ 1576  g++ -std=c++20 -pthread -o preguntaMultiThreads texto_plano_threads.cpp
+ 1577  g++ -std=c++20 -pthread -o preguntaMultiThreads.txt texto_plano_threads.cpp
+ 1578  ./preguntaMultiThreads
+ 1579  ./a.out
+ 1580  clear
+ 1581  git add .
+ 1582  git commit -m "Pregunta 4"
+ 1583  git push
+ 1584  git remote add ExamenParcial1 https://github.com/RHNaranjo/ExamenParcial1
+ 1585  git push ExamenParcial1
+ 1586  git push --set-upstream ExamenParcial1 main
+ 1587  clear
+ 1588  ls
+ 1589  cd ..
+ 1590  ls
+ 1591  cd ExamenParcial1
+ 1592  ls
+ 1593  touch accept.txt; ls >> accept.txt && history | grep ls >> accept.txt
